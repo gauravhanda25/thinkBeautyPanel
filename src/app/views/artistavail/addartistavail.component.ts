8a41419b019b8891bb773ff4d2108565ee1d351d
megha <megha@aroracomfortechs.com>
2018-03-07 16:49:24 +0530

    fixes

--- src/app/views/artistavail/addartistavail.component.ts
+++ src/app/views/artistavail/addartistavail.component.ts
@@ -142,7 +142,7 @@ export class AddartistavailComponent {
 
 	    	this.http.post(API_URL+'/Artistavailabilities?access_token='+ localStorage.getItem('currentUserToken'), savedata, options)
 	      .subscribe(response => {
-          if(dayno == 6) {
+          if(dayno == "6") {
 				    this.toasterService.pop('success', 'Success', "Availability saved successfully"); 
 
             this.getAllAvailData(); 
@@ -175,7 +175,7 @@ export class AddartistavailComponent {
 
 	    	this.http.post(API_URL+'/Artistavailabilities/update?where=%7B%22and%22%3A%5B%7B%22artistId%22%3A%22'+localStorage.getItem('currentUserId')+'%22%7D%2C%7B%22day%22%3A%22'+savedata.day+'%22%7D%5D%7D&access_token='+ localStorage.getItem('currentUserToken'), savedata, options)
         .subscribe(response => {
-				  if(dayno == 6) {
+				  if(dayno == "6") {
             this.toasterService.pop('success', 'Success', "Availability updated successfully"); 
 
             this.getAllAvailData();
