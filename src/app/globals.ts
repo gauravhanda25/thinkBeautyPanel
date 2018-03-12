12ffa6156d17dcb6c50bd2227f065c617bc0b77c
Gaurav Handa <gaurav@aroracomfortechs.com>
2018-03-12 23:47:50 +0530

    Adding new image

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,5 +3,5 @@
 //
 'use strict';
 
-export const API_URL: string = 'http://localhost:3000/api';  
-//export const API_URL: string = 'http://18.219.102.221:3000/api';  
+//export const API_URL: string = 'http://localhost:3000/api';  
+export const API_URL: string = 'http://www.thinkbeauty.net:3000/api';  
