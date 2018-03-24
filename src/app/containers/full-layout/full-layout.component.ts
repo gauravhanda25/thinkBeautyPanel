324aa5502094cec077557185e0016eef2d2fc46b
Gaurav Handa <gaurav@aroracomfortechs.com>
2018-03-24 11:40:15 +0530

    Fixed menu issues

--- src/app/containers/full-layout/full-layout.component.ts
+++ src/app/containers/full-layout/full-layout.component.ts
@@ -11,12 +11,128 @@ import { NgxPermissionsService, NgxRolesService, NgxPermissionsDirective } from 
 export class FullLayoutComponent { 
 
 	private display:any;
+	private navigation:any = [];
 	constructor(private NgxRolesService: NgxRolesService, private NgxPermissionsService: NgxPermissionsService) {
 
 	    if(localStorage.getItem('currentUserRoleId') == "1"){
 	      localStorage.setItem('currentUserRole', "ADMIN");
+	      this.navigation = [{
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+  },
+  {
+    name: 'Manage Artists',
+    url: '/manageartist',
+    icon: 'icon-people',
+    children: [
+      {
+        name: 'New Artist Requests',
+        url: '/manageartist/newrequests',
+        icon: 'icon-people',
+      },
+      {
+        name: 'Registered Artists',
+        url: '/manageartist/registered',
+        icon: 'icon-people',
+      },
+      {
+        name: 'Rejected Artists',
+        url: '/manageartist/rejected',
+        icon: 'icon-people',
+      },
+      {
+        name: 'Add New Artist',
+        url: '/manageartist/addartist',
+        icon: 'icon-people',
+      },
+    ]
+  }, 
+    {
+    name: 'Manage Salons',
+    url: '/managesalon',
+    icon: 'icon-list',
+          children: [
+      {
+    name: 'New Salon Requests',
+    url: '/managesalon/newrequests',
+    icon: 'icon-list',
+  },
+    {
+    name: 'Registered Salon',
+    url: '/managesalon/registered',
+    icon: 'icon-list',
+  },
+    {
+    name: 'Rejected Salon',
+    url: '/managesalon/rejected',
+    icon: 'icon-list',
+  },
+      {
+    name: 'Add New Salon',
+    url: '/managesalon/addsalon',
+    icon: 'icon-list',
+  },
+      ]
+  }, 
+
+  {
+    name: 'Services',
+    url: '/',
+    icon: 'icon-settings',
+    children: [
+      {
+        name: 'Makeup',
+        url: '/makeup',
+        icon: 'icon-list',
+      },
+      {
+        name: 'Hair',
+        url: '/hair',
+        icon: 'icon-list',
+      },
+      {
+        name: 'Nails',
+        url: '/nails',
+        icon: 'icon-list',
+      }
+    ]
+  }];
 	    } else if(localStorage.getItem('currentUserRoleId') == "2"){
 	      localStorage.setItem('currentUserRole', "ARTIST");
+	      this.navigation = [{
+		    name: 'Dashboard',
+		    url: '/dashboard',
+		    icon: 'icon-speedometer',
+		  },
+		  {
+		    name: 'My Services',
+		    url: '/myservices',
+		    icon: 'icon-list',
+		  },
+		  {
+		    name: 'Schedule',
+		    url: '/schedule',
+		    icon: 'icon-clock',
+		    children: [
+		  
+		      {
+		        name: 'Work Availability',
+		        url: '/schedule/work',
+		        icon: 'icon-clock',
+		      },
+		      {
+		        name: 'Vacations',
+		        url: '/schedule/vacation',
+		        icon: 'icon-clock',
+		      },
+		      {
+		        name: 'GCC Availability',
+		        url: '/schedule/gcc',
+		        icon: 'icon-clock',
+		      }
+		    ]
+		  }];
 	    } else if(localStorage.getItem('currentUserRoleId') == "3"){
 	      localStorage.setItem('currentUserRole', "SALON");
 	    } 
@@ -39,6 +155,17 @@ export class FullLayoutComponent {
 	    } else if(localStorage.getItem('loader') == "false"){
 	      this.display = 0;
 	    } 
+	    
+
+  
+  }
+
+  public isDivider(item) {
+    return item.divider ? true : false
+  }
+
+  public isTitle(item) {
+    return item.title ? true : false
   }
 
 
