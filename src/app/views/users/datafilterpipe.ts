53702c119ad5bbf4473ed5d53f5593ca7791927b
Megha <er.megha9207@gmail.com>
2018-04-14 19:52:32 +0530

    user fixes

--- src/app/views/users/datafilterpipe.ts
+++ src/app/views/users/datafilterpipe.ts
@@ -9,7 +9,7 @@ export class DataFilterPipe implements PipeTransform {
   transform(array: any[], query: string): any {
     if (query) {
    	  console.log(query);
-      return _.filter(array, row=>((row.name.toLowerCase().indexOf(query.toLowerCase()) > -1) (row.countryname.toLowerCase().indexOf(query.toLowerCase()) > -1)  );  
+      return _.filter(array, row=>((row.fullName.toLowerCase().indexOf(query.toLowerCase()) > -1) (row.countryname.toLowerCase().indexOf(query.toLowerCase()) > -1)  );  
       
     }
     return array;
