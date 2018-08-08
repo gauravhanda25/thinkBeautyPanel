41400fd9ae064628a78da8ac4e5ed8f21f288309
megha <er.megha9207@gmail.com>
2018-08-08 14:25:17 +0530

    api url changes of https

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -4,4 +4,4 @@
 'use strict';
 
 //export const API_URL: string = 'http://localhost:3000/api';  
-export const API_URL: string = 'http://www.thinkbeauty.net:3000/api';  
+export const API_URL: string = 'https://www.thinkbeauty.net:3000/api';  
